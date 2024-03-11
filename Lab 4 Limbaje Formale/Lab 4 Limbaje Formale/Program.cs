ea35d2e1f06f18db83bc18057f0f6aedb01b5aa2
Cosmiuc Razvan Ionut <116715647+ricosmi@users.noreply.github.com>
2024-03-11 18:12:19 +0200

    Add files via upload

---
+++ Lab 4 Limbaje Formale/Lab 4 Limbaje Formale/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+
+class Problema_1
+{
+    public static char RunSequence(string sequence)
+    {
+        char currentState = 'A'; 
+        foreach (char symbol in sequence)
+        {
+            switch (currentState)
+            {
+                case 'A':
+                    if (symbol == '0')
+                    {
+                        currentState = 'B'; 
+                    }
+                    else
+                    {
+                        currentState = 'A'; 
+                    }
+                    break;
+                case 'B':
+                    if (symbol == '0')
+                    {
+                        currentState = 'A'; 
+                    }
+                    else
+                    {
+                        currentState = 'B'; 
+                    }
+                    break;
+            }
+
+        }
+
+        return currentState;
+    }
+
+    static void Main(string[] args)
+    {
+        string secventa = Console.ReadLine();
+        char state=RunSequence(secventa);
+        Console.WriteLine("Stare finala: "+state);
+    }
+}
